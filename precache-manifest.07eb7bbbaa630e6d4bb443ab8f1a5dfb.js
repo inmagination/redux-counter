1ea86af24a903b945aa77df3dd7acca01cceb051
inmaculada.reyes <inmaculada.reyes@softtek.com>
2019-04-15 11:55:14 +0200

    Updates

---
+++ precache-manifest.07eb7bbbaa630e6d4bb443ab8f1a5dfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0078c8eb852a8f895f9",
+    "url": "/redux-counter/static/js/runtime~main.125d2423.js"
+  },
+  {
+    "revision": "7e344b9bb8c27a82f542",
+    "url": "/redux-counter/static/js/main.ef9e8067.chunk.js"
+  },
+  {
+    "revision": "b029bbc547ef7d1f0909",
+    "url": "/redux-counter/static/js/2.312033a9.chunk.js"
+  },
+  {
+    "revision": "7e344b9bb8c27a82f542",
+    "url": "/redux-counter/static/css/main.30453ab1.chunk.css"
+  },
+  {
+    "revision": "ced39a862d6d6427c689ff78e1b389a4",
+    "url": "/redux-counter/index.html"
+  }
+];
